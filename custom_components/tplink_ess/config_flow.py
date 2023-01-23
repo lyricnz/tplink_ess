935d768d355f702fc594b49e9bde7c452c9c4fbf
Chris <firstof9@gmail.com>
2023-01-22 17:56:24 -0700

    use the correct dict for saving config

--- custom_components/tplink_ess/config_flow.py
+++ custom_components/tplink_ess/config_flow.py
@@ -120,7 +120,7 @@ class TPLinkESSFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             if valid:
                 self._data.update(user_input)
                 return self.async_create_entry(
-                    title=user_input[CONF_MAC], data=user_input
+                    title=self._data[CONF_MAC], data=self._data
                 )
             self._errors["base"] = "auth"
             return await self._show_config_form(user_input)
