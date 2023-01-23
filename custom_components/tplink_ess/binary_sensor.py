679664b0722b894146f09602e002504c63383cc3
Chris <firstof9@gmail.com>
2023-01-23 10:39:11 -0700

    add more binary sensors

--- custom_components/tplink_ess/binary_sensor.py
+++ custom_components/tplink_ess/binary_sensor.py
@@ -1,14 +1,12 @@
 """Binary sensor platform for integration_blueprint."""
 import logging
 
-from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.binary_sensor import BinarySensorDeviceClass, BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-)
+from homeassistant.helpers.entity import EntityCategory
+from homeassistant.helpers.update_coordinator import (DataUpdateCoordinator)
 
-from .const import BINARY_SENSOR, BINARY_SENSOR_DEVICE_CLASS, DEFAULT_NAME, DOMAIN
+from .const import BINARY_SENSOR_DEVICE_CLASS, DOMAIN
 from .entity import TPLinkESSEntity
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
@@ -24,45 +22,81 @@ async def async_setup_entry(hass, entry, async_add_devices):
     i = 0
     while i < limit:
         binary_sensors.append(
-            TPLinkESSBinarySensor(i, coordinator, entry)
+            TPLinkESSBinarySensor(i, None, coordinator, entry)
         )
         i = i + 1
+
+    # one off binary sensors
+    binary_sensors.append(TPLinkESSBinarySensor(None, "qos1", coordinator, entry))
+    binary_sensors.append(TPLinkESSBinarySensor(None, "loop_prev", coordinator, entry))
+    binary_sensors.append(TPLinkESSBinarySensor(None, "dhcp", coordinator, entry))
+    
     async_add_devices(binary_sensors, False)
 
 
 class TPLinkESSBinarySensor(TPLinkESSEntity, BinarySensorEntity):
     """TPLink ESS binary_sensor class."""
 
-    def __init__(self, port: int, coordinator: DataUpdateCoordinator, config: ConfigEntry) -> None:
+    def __init__(self, port: int | None, key: str | None, coordinator: DataUpdateCoordinator, config: ConfigEntry) -> None:
         """Initialize."""
         super().__init__(coordinator, config)
         self.coordinator = coordinator
         self._config = config
-        self._port = port
-        self._data = coordinator.data.get("stats")["stats"][port]
-        self._name = self._data["Port"]
-
-        self._attr_name = f"Port {self._name}"
-        self._attr_unique_id = f"port{port}_{self._config.entry_id}"
+        self._port = None
+        self._key = None
+        if port is not None:
+            self._data = coordinator.data.get("stats")["stats"][port]
+            self._port = port
+            self._name = self._data["Port"]
+            self._attr_name = f"Port {self._name}"
+            self._attr_unique_id = f"port{port}_{self._config.entry_id}"
+        if key is not None:
+            if key == "dhcp":
+                self._data = coordinator.data.get("hostname")[key]
+            else:
+                self._data = coordinator.data.get(key)[key]
+            self._key = key
+            self._attr_name = key
+            self._attr_unique_id = f"{key}_{self._config.entry_id}"
 
     @property
     def device_class(self):
         """Return the class of this binary_sensor."""
-        return BINARY_SENSOR_DEVICE_CLASS
+        if self._port is not None:
+            return BinarySensorDeviceClass.CONNECTIVITY
+        return BinarySensorDeviceClass.RUNNING
+
+    @property
+    def entity_category(self):
+        """Return the category of this binary_sensor."""
+        if self._key is not None:
+            return EntityCategory.DIAGNOSTIC
+        return None  
 
     @property
     def is_on(self):
         """Return true if the binary_sensor is on."""
-        return self._data["Link Status Raw"]
+        if self._data is None:
+            _LOGGER.warning("Data missing for: %s", self._attr_name)
+            return False
+        if self._port is not None:
+            return self._data["Link Status Raw"]
+        if self._key is not None:
+            return self._data
+        return False
 
     @property
     def available(self) -> bool:
         """Return if entity is available."""
-        if "stats" in self.coordinator.data.get("stats"):
+        if self._port is not None and "stats" in self.coordinator.data.get("stats"):
+            return True
+        if self._key is not None and self._key in self.coordinator.data or self._key in self.coordinator.data.get("hostname"):
             return True
         return False
 
     @property
     def extra_state_attributes(self):
         """Return the state attributes."""
-        return self._data
+        if self._port is not None:
+            return self._data
+        return None
