d6fa134860b4c13700fbc292d5e62634b034748d
Chris <firstof9@gmail.com>
2023-01-29 16:13:20 -0700

    clean slate

--- custom_components/tplink_ess/const.py
+++ custom_components/tplink_ess/const.py
@@ -1,7 +1,4 @@
 """Constants for tplink_ess."""
-from homeassistant import const
-from homeassistant.const import Platform
-
 # Base component constants
 NAME = "TPLink ESS"
 DOMAIN = "tplink_ess"
@@ -13,11 +10,6 @@ ICON = "mdi:format-quote-close"
 
 # Platforms
 PLATFORMS = ["binary_sensor", "sensor"]
-BINARY_SENSOR = Platform.BINARY_SENSOR
-SENSOR = Platform.SENSOR
-SWITCH = Platform.SWITCH
-CONF_PASSWORD = const.CONF_PASSWORD
-CONF_USERNAME = const.CONF_USERNAME
 
 # Defaults
 DEFAULT_NAME = DOMAIN
