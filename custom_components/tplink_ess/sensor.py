f243f2e0ddbe1c3b9b90e8b7a76db352d6dd77f5
Simon Roberts <lyricnz@gmail.com>
2023-01-31 12:22:52 +1100

    Fix stats sensors

--- custom_components/tplink_ess/sensor.py
+++ custom_components/tplink_ess/sensor.py
@@ -138,7 +138,7 @@ class TPLinkESSSensor(TPLinkESSEntity, SensorEntity):
             return data.get("vlan")["vlan"][self._item_id]["VLAN ID"]
         if self._key == "pvid":
             return data.get("pvid")["pvid"][self._item_id][1]
-        if value := data.get("stats")["stats"][self._item_id].get(self._key) is not None:
+        if (value := data.get("stats")["stats"][self._item_id].get(self._key)) is not None:
             if self._key in ("TxGoodPkt", "RxGoodPkt"):
                 return int(value)
             return value
