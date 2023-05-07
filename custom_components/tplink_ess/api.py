44887e8eb2234380e03d46f762bee6398ea2254a
Simon Roberts <lyricnz@gmail.com>
2023-05-07 12:47:52 +1000

    Only request the data from the switch that is being used by this integration

--- custom_components/tplink_ess/api.py
+++ custom_components/tplink_ess/api.py
@@ -9,6 +9,7 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 class TPLinkESSClient:
     """Representation of a TPLink ESS network switch."""
+    ACTION_NAMES = {'hostname', 'vlan', 'pvid', 'num_ports', 'stats'}
 
     def __init__(
         self,
@@ -25,7 +26,9 @@ class TPLinkESSClient:
     async def async_get_data(self) -> dict:
         """Get data from the API."""
         api = TpLinkESS(self._host_mac, self._username, self._password)
-        return await api.update_data(switch_mac=self._switch_mac)
+        return await api.update_data(
+            switch_mac=self._switch_mac,
+            action_names=TPLinkESSClient.ACTION_NAMES)
 
     async def async_discover_switches(self) -> list:
         """Discover switches and return results."""
